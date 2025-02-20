94482b472e116f3e99812126bdcf81653d42e6b7
Samz-9 <samyakjain08973@gmail.com>
2025-02-20 18:22:29 +0530

    Spotify

--- Spotify Clone/spotify.js
+++ Spotify Clone/spotify.js
@@ -1,6 +1,6 @@
 
 async function getsongs() {
-    let a = await fetch("http://127.0.0.1:3000/Spotify clone/songs/")
+    let a = await fetch("http://127.0.0.1:3000/Spotify%20Clone/songs/")
     let response = await a.text();
     console.log(response);
     let div = document.createElement("div")
